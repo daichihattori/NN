8ec602e51b03f5be20a5403929194e4d005df2da
Daichi Hattori <d_hattori@eaglys.co.jp>
2024-05-01 15:29:28 +0900

    added and, or, nand

---
+++ test/test_perceptron.py
@@ -0,0 +1,19 @@
+import pytest
+from src import perceptron
+def test_AND():
+    assert perceptron.AND(0, 0) == 0
+    assert perceptron.AND(0, 1) == 0
+    assert perceptron.AND(1, 0) == 0
+    assert perceptron.AND(1, 1) == 1
+
+def test_OR():
+    assert perceptron.OR(0, 0) == 0
+    assert perceptron.OR(0, 1) == 1
+    assert perceptron.OR(1, 0) == 1
+    assert perceptron.OR(1, 1) == 1
+
+def test_NAND():
+    assert perceptron.NAND(0, 0) == 1
+    assert perceptron.NAND(0, 1) == 1
+    assert perceptron.NAND(1, 0) == 1
+    assert perceptron.NAND(1, 1) == 0
